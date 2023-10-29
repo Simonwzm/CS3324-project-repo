c92ea52bc4e8f6cfb94f88d5b908b05a7afb8e1a
Simon <simon_wzm@163.com>
2023-10-29 11:16:24 +0800

    rm cache

--- test_train.py
+++
@@ -1,5 +0,0 @@
-import pickle
-with open('./datasets/meta_info/.pkl', 'rb') as f:
-    meta_info = pickle.load(f)
-    print(meta_info.keys())
-    print([[len(meta_info[y][x]) for x in meta_info[y].keys()] for y in meta_info.keys()])
